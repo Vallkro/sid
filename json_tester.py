5dd8dacfb43abf1bc6730053c49f5b06b6e82631
Vallkro <kronav@student.chalmers.se>
2021-08-16 12:47:56 +0200

    config file

---
+++ json_tester.py
@@ -0,0 +1,13 @@
+import json
+import os
+# Look for your absolute directory path
+absolute_path = os.path.dirname(os.path.abspath(__file__))
+print(absolute_path)
+file_path = os.path.join(absolute_path, 'config.json')
+print(file_path)
+
+f =open(file_path) 
+config = json.load(f)
+#f.close()
+print(config['robot_command_topic'])
+f.close()
--- subscriber_member_function.py
+++ subscriber_member_function.py
@@ -2,21 +2,30 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
-
+from geometry_msgs.msg import Twist
+import json
+import os
 
 class MinimalSubscriber(Node):
 
     def __init__(self):
+        # Load json config
+        #absolute_path = os.path.dirname(os.path.abspath(__file__))
+        #file_path = os.path.join(absolute_path, 'config.json')
+        file_path = '/home/valle/dev_ws/src/sid_pubsub/sid_pubsub/config.json'
+        f =open(file_path) 
+        config = json.load(f)
+        f.close()
         super().__init__('minimal_subscriber')
         self.subscription = self.create_subscription(
-            String,
-            'topic',
+            Twist,
+            config['robot_command_topic'],
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.get_logger().info('I heard: "%s"' % msg)
 
 
 def main(args=None):
